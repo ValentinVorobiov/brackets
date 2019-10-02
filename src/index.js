ea424f9916c01a866fe4df9382a2cff28397d4ce
Varabiou Valiantsin <vxdator@gmail.com>
2019-10-02 22:41:47 +0300

    feat: done Brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 module.exports = function check(str, bracketsConfig) {
+
+    function MakeHumanBrackets( srcBracketsConfig ){
+        return srcBracketsConfig.map( ( configArray ) => {
+            return configArray.join( '' );
+        } );
+    }
+
+    function ReduceBrackets( strSource, humanBracketsCfg ){
+        let strResult = strSource;
+
+        for( curBracketsIndex = 0; curBracketsIndex < humanBracketsCfg.length; curBracketsIndex++  ){
+            let currentBrackets = humanBracketsCfg[ curBracketsIndex ];
+            for( let charIndex = 0; charIndex < strResult.length;  ){
+
+                if( strResult.indexOf( currentBrackets ) > -1 ){
+
+                    strResult = strResult.replace( currentBrackets, '' );
+                    charIndex = -1;
+                    curBracketsIndex = -1;
+
+                } else {
+
+                    charIndex = strResult.length;
+
+                }
+
+            }
+
+        }
+
+        return strResult;
+    }
+
+
+    let humanBracketsConfig = MakeHumanBrackets( bracketsConfig );
+    let strCleared = ReduceBrackets( str, humanBracketsConfig );
+
+    return( !strCleared.length );
+
+
   // your solution
 }
